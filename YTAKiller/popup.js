698cb2b2c42db0a3596192a4a7aeaaa0de8b49c0
kgresmer <kgresmer@gmail.com>
2018-07-17 12:47:55 -0500

    Killing ads on youtube navigation now. May not be consistent.

--- YTAKiller/popup.js
+++ YTAKiller/popup.js
@@ -1,19 +1,30 @@
 
 setTimeout(function(){
     console.log("Running youtube overlay killer");
+    killAds();
+}, 1500);
 
+
+
+window.addEventListener('yt-navigate-start', function (e) {
+    console.log('yt nav start');
+    setTimeout(function(){
+        console.log("Running youtube overlay killer");
+        killAds();
+    }, 1500);
+}, false);
+
+function killAds() {
     var ytdPlayer = document.getElementById('ytd-player');
     var parent = ytdPlayer.children[0];
     for (var i = 0; i < parent.children.length; i++) {
         var child = parent.children[i];
-        if (child.className.indexOf('html5-video-container') === -1) {
+        if (child.className.indexOf('html5-video-container') === -1 &&
+            child.className.indexOf('ytp-chrome-bottom') === -1) {
             parent.removeChild(child);
         }
     }
-
-
-
-}, 2000);
+}
 
 
 
